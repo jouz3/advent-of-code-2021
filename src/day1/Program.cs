07366c6204cfe8cfcab7d73b401f13c78620eea5
jouz3 <jouzefcp@me.com>
2021-12-07 19:50:13 +0000

    feat: added day 1 part two

--- src/day1/Program.cs
+++ src/day1/Program.cs
@@ -1,16 +1,37 @@
 ﻿var increases = 0;
+var y = 0;
 int? previousRecord = default;
 var input = await File.ReadAllLinesAsync("input.csv");
 
-foreach (var record in input)
-{
-    if (int.TryParse(record, out int currentRecord))
-    {
-        if (currentRecord > (previousRecord ?? currentRecord))
-            increases++;
+// Parse input to integers collection
+var inputParsed = input
+    .Where(i => int.TryParse(i, out y))
+    .Select(i => y);
 
-        previousRecord = currentRecord;
-    }
+foreach (var record in inputParsed)
+{
+    if (record > (previousRecord ?? record))
+        increases++;
+
+    previousRecord = record;
 }
 
-Console.WriteLine($"Number of increases: {increases}");
+Console.WriteLine($"[Challenge part 1] Number of increases: {increases}");
+
+// Reset increase number
+increases = 0;
+
+// Build three-measurement sliding window
+var inputWindows = inputParsed
+    .Zip(inputParsed.Skip(1), (current, next1) => new { current, next1})
+    .Zip(inputParsed.Skip(2), (current, next2) => current.current + current.next1 + next2);
+
+foreach (var record in inputWindows)
+{
+    if (record > (previousRecord ?? record))
+        increases++;
+
+    previousRecord = record;
+}
+
+Console.WriteLine($"[Challenge part 2] Number of increases: {increases}");
