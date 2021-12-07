ed3cafc9580edabc6fffc4aab609b2428e38d913
jouz3 <jouzefcp@me.com>
2021-12-07 19:03:14 +0000

    feat: added day1 challenge

---
+++ src/day1/Program.cs
@@ -0,0 +1,16 @@
+﻿var increases = 0;
+int? previousRecord = default;
+var input = await File.ReadAllLinesAsync("input.csv");
+
+foreach (var record in input)
+{
+    if (int.TryParse(record, out int currentRecord))
+    {
+        if (currentRecord > (previousRecord ?? currentRecord))
+            increases++;
+
+        previousRecord = currentRecord;
+    }
+}
+
+Console.WriteLine($"Number of increases: {increases}");
