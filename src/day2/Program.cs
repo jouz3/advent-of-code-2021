9a70143de6842965731f1bf84e8b9b5aa2bd09ac
jouz3 <jouzefcp@me.com>
2021-12-08 13:54:35 +0000

    feat: added day 2 challenge

---
+++ src/day2/Program.cs
@@ -0,0 +1,66 @@
+﻿var horizontalPosition = 0;
+var depth = 0;
+
+var input = await File.ReadAllLinesAsync("input.csv");
+
+var inputParsed = input
+    .Select(i => new 
+    { 
+        Command = i?.Split(' ').First(), 
+        Value = i?.Split(' ').Last() 
+    });
+
+foreach (var record in inputParsed)
+{
+    if (int.TryParse(record.Value, out int valueInt))
+    {
+        switch (record.Command)
+        {
+            case "forward":
+                horizontalPosition += valueInt;
+                break;
+            case "down":
+                depth += valueInt;
+                break;
+            case "up":
+                depth -= valueInt;
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+Console.WriteLine($"Horizontal Position: {horizontalPosition} Depth: {depth}");
+Console.WriteLine($"[Challenge part 1] Final Position: {horizontalPosition * depth}");
+
+// Reset values
+horizontalPosition = 0;
+depth = 0;
+
+var aim = 0;
+
+foreach (var record in inputParsed)
+{
+    if (int.TryParse(record.Value, out int valueInt))
+    {
+        switch (record.Command)
+        {
+            case "forward":
+                horizontalPosition += valueInt;
+                depth += aim * valueInt;
+                break;
+            case "down":
+                aim += valueInt;
+                break;
+            case "up":
+                aim -= valueInt;
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+Console.WriteLine($"Horizontal Position: {horizontalPosition} Depth: {depth}");
+Console.WriteLine($"[Challenge part 2] Final Position: {horizontalPosition * depth}");
